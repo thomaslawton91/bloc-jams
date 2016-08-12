699074682cad0abdafce448ad6f37d6e42818f11
Thomas Lawton <thomaslawton91@gmail.com>
2016-08-12 16:37:18 -0400

    Add new album cover to assets. Add new album to album.js and update to add click event listener to albumImage element.

--- scripts/album.js
+++ scripts/album.js
@@ -28,38 +28,64 @@ var albumMarconi = {
     ]
 };
 
-var createSongRow = function(songNumber, songName, songLength) {
-    var template = 
-        '<tr class="album-view-song-item">'
-        +'  <td class="song-item-number">' + songNumber + '</td>'
-        +'  <td class="song-item-title">' + songName + '</td>'
-        +'  <td class="song-item-duration">' + songLength + '</td>'
-        +'</tr>'
-        ;
-    
-    return template;
+var albumDog = {
+    title: 'Always Barking',
+    artist: 'The Dog Nextdoor',
+    label: 'Hell',
+    year: 'Forever',
+    albumArtUrl: 'assets/images/album_covers/dog.png',
+    songs: [
+        {title: 'woof', duration: '10:00'},
+        {title: 'arf', duration: '12:00'},
+        {title: 'bark', duration: '11:00'},
+        {title: 'squeak', duration: '0:40'},
+        {title: ' . . . ', duration: '0:00'}
+    ]
 };
+
+ var createSongRow = function(songNumber, songName, songLength) {
+     var template =
+        '<tr class="album-view-song-item">'
+      + '  <td class="song-item-number">' + songNumber + '</td>'
+      + '  <td class="song-item-title">' + songName + '</td>'
+      + '  <td class="song-item-duration">' + songLength + '</td>'
+      + '</tr>'
+      ;
+ 
+     return template;
+ };
+
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
 var setCurrentAlbum = function(album) {
-    
-    var albumTitle = document.getElementsByClassName('album-view-title')[0];
-    var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-    var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-    var albumImage = document.getElementsByClassName('album-cover-art')[0];
-    var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
-    
-    albumTitle.firstChild.nodeValue = album.title;
-    albumArtist.firstChild.nodeValue = album.artist;
-    albumReleaseInfo.firstChild.nodeValue = album.year + '' + album.label;
-    albumImage.setAttribute('src', album.albumArtUrl);
-    
-    albumSongList.innerHTML = '';
-    
-    for (var i = 0; i < album.songs.length; i++) {
-        albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
-    }
-};
+ 
+     albumTitle.firstChild.nodeValue = album.title;
+     albumArtist.firstChild.nodeValue = album.artist;
+     albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
+     albumImage.setAttribute('src', album.albumArtUrl);
+ 
+     albumSongList.innerHTML = '';
+ 
+     for (var i = 0; i < album.songs.length; i++) {
+         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+     }
+ };
 
 window.onload = function() {
+    
     setCurrentAlbum(albumPicasso);
+    
+    var albums = [albumPicasso, albumMarconi, albumDog];
+    var index = 1;
+    albumImage.addEventListener('click', function(event) {
+        setCurrentAlbum(albums[index]);
+        index++;
+        if (index == albums.length) {
+            index = 0;
+        }
+    });
 };
