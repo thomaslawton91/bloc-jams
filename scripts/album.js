453b702d8a1e0e133e791e396db9f62e1f1785a6
Thomas Lawton <thomaslawton91@gmail.com>
2016-09-12 19:54:02 -0400

    Refactor album info creation in album.js for JQuery. Refactor template creator in collection.js for JQuery. Add JQuery script to album and collection .html.

--- scripts/album.js
+++ scripts/album.js
@@ -52,7 +52,7 @@ var albumDog = {
       + '</tr>'
       ;
  
-     return template;
+     return $(template);
  };
 
 var albumTitle = document.getElementsByClassName('album-view-title')[0];
@@ -63,15 +63,22 @@ var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
 var setCurrentAlbum = function(album) {
  
-     albumTitle.firstChild.nodeValue = album.title;
-     albumArtist.firstChild.nodeValue = album.artist;
-     albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
-     albumImage.setAttribute('src', album.albumArtUrl);
- 
-     albumSongList.innerHTML = '';
+    var $albumTitle = $('.album-view-title');
+    var $albumArtist = $('.album-view-artist');
+    var $albumReleaseInfo = $('.album-view-release-info');
+    var $albumImage = $('.album-cover-art');
+    var $albumSongList = $('.album-view-song-list');
+    
+    $albumTitle.text(album.title);
+    $albumArtist.text(album.artist);
+    $albumReleaseInfo.text(album.year + '' + album.label);
+    $albumImage.attr('src', album.albumArtUrl);
+    
+    $albumSongList.empty();
  
      for (var i = 0; i < album.songs.length; i++) {
-         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+         var $newRow = createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+         $albumSongList.append($newRow);
      }
  };
 
