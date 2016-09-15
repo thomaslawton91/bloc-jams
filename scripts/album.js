d87cf7fe6a9d7918e724d7ec0af32de132f8638e
Thomas Lawton <thomaslawton91@gmail.com>
2016-09-15 13:28:17 -0400

    Add BuzzMusic api. Add links to mp3 in Picasso album. Add volume and music functionality to album.js.

--- scripts/album.js
+++ scripts/album.js
@@ -1,7 +1,28 @@
 var setSong = function(songNumber) {
+    
+    if(currentSoundFile) {
+        currentSoundFile.stop();
+    }
+    
     currentlyPlayingSongNumber = parseInt(songNumber);
     currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+    
+    currentSoundFile = new buzz.sound(currentSongFromAlbum.audioUrl, {
+        
+        formats: ['mp3'],
+        preload: true
+        
+    });
+    
+    setVolume(currentVolume);
 };
+
+var setVolume = function(volume) {
+    
+    if (currentSoundFile) {
+        currentSoundFile.setVolume(volume);
+    }
+}
 
 var getSongNumberCell = function(number) {
     return $('.song-item-number[data-song-number="' + number + '"]');
@@ -28,11 +49,19 @@ var createSongRow = function(songNumber, songName, songLength) {
 	}   if (currentlyPlayingSongNumber !== songNumber) {
 		$(this).html(pauseButtonTemplate);
 		setSong(songNumber);
+        currentSoundFile.play();
         updatePlayerBarSong();
 	}  else if (currentlyPlayingSongNumber === songNumber) {
-		$(this).html(playButtonTemplate);
-        $('.main-controls .play-pause').html(playerBarPlayButton);
-		setSong(songNumber);
+        
+            if (currentSoundFile.isPaused()) {
+                $(this).html(pauseButtonTemplate);
+                $('.main-controls .play-pause').html(playerBarPauseButton);
+                currentSoundFile.play();
+            } else {
+                $(this).html(playButtonTemplate);
+                $('.main-controls .play-pause').html(playerBarPlayButton);
+                currentSoundFile.pause();
+            }
 	}
 };
      
@@ -106,6 +135,8 @@ var nextSong = function() {
     }
     
     setSong(currentSongIndex + 1);
+    currentSoundFile.play();
+    updatePlayerBarSong();
     
     $('.currently-playing .song-name').text(currentSongFromAlbum.title);
     $('.currently-playing .artist-name').text(currentAlbum.artist);
@@ -135,6 +166,8 @@ var previousSong = function() {
     }
     
     setSong(currentSongIndex + 1);
+    currentSoundFile.play();
+    updatePlayerBarSong();
     
     $('.currently-playing .song-name').text(currentSongFromAlbum.title);
     $('.currently-playing .artist-name').text(currentAlbum.artist);
@@ -167,6 +200,8 @@ var playerBarPauseButton = '<span class="ion-pause"></span>';
 var currentAlbum = null;
 var currentlyPlayingSongNumber = null;
 var currentSongFromAlbum = null;
+var currentSoundFile = null;
+var currentVolume = 80;
 
 var $previousButton = $('.main-controls .previous');
 var $nextButton = $('.main-controls .next');
